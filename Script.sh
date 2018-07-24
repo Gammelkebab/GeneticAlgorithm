841284f8cc6fd8feba2c13d3c6c191b5854d0c4a
Aria Givi <ariagivi@yahoo.de>
2018-07-24 13:59:08 +0200

    Ausführung auf Lena-Nodes reingeschrieben

--- Script.sh
+++ Script.sh
@@ -2,9 +2,10 @@
 #PBS -N GeneticAlgorithm
 #PBS -j oe
 #PBS -m ae
-#PBS -l nodes=1:ppn=16
+#PBS -l nodes=4:ppn=4
 #PBS -l walltime=00:20:00
 #PBS -l mem=4gb
+#PBS -W x=PARTITION:lena
 
 module load intel
 
