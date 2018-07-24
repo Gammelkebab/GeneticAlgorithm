ed7f01c9e4439cc004d51d8506e0c2d72d0d390a
Aria Givi <ariagivi@yahoo.de>
2018-07-24 13:22:13 +0200

    Script Namen geändert

---
+++ Genetic_Algorithm_Benchmark/Benchmark_Script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -login
+#PBS -N GeneticAlgorithm_Benchmark
+#PBS -j oe
+#PBS -m ae
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=00:20:00
+#PBS -l mem=4gb
+#PBS -o benchmark_log.txt
+
+module load intel
+
+cd GeneticAlgorithm/Genetic_Algorithm_Benchmark
+
+make
+
+./main
